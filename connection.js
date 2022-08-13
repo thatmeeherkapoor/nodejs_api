f650e04b8068852f89260e00cd994f1de9487056
thatmeeherkapoor <mformeeher@gmail.com>
2022-08-13 20:28:04 +0300

    Adding mongoDB connection in index.js and then also adding back the connection.js file for further future reference.

--- connection.js
+++ connection.js
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
-import promptSync from 'prompt-sync'; 
 
-const prompt = promptSync(); 
-const password = prompt("what is the database password?");
+//Auth string that is used to connect to the database. 
+const uri = "mongodb+srv://nodejs-server-trial-m:meeher23@cluster0.8rwavg2.mongodb.net/?retryWrites=true&w=majority";
 
-const uri = "mongodb+srv://nodejs-server-trial-m:" + password + "@cluster0.8rwavg2.mongodb.net/?retryWrites=true&w=majority"
-
+//Async function -> Allows us to run other things while the connect function runs. 
 async function connect() {
     try {
+        //If connected, output.
         await mongoose.connect(uri);
         console.log("connected to mongodb")
     } catch (error) {
+        //Else display the error.
         console.log(error)
     }
  }
 
+//Call the connect method. 
  connect();
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+//Important libraries. 
 import express from "express";
+import mongoose from "mongoose";
+
+//Create the auth string responsible for connecting to the database. 
+const uri = "mongodb+srv://nodejs-server-trial-m:meeher23@cluster0.8rwavg2.mongodb.net/?retryWrites=true&w=majority";
 
 //Initial configuration of the server.
 const app = express(); 
+
+//Connect to the database using the uri link. 
+mongoose.connect(uri)
+        .then((res) => console.log("connected to mongo"))
+        .catch((error) => console.log(error));
 
 /**
  * The application can send a request and get a response on the /user endpoint.
@@ -9,8 +19,6 @@ const app = express();
  * res - what you are getting on the webserver.
  * req - what you are sending on the webserver. 
 */
-
-
 app.get("/user", (req, res) => { 
     //The response sends a HTML header tag showing hello world, when accessed in the browser using the /user endpoint. 
     res.send("<h1>Hello World !!! (:</h1>");
